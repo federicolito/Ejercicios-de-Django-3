fc3526eafbccab20595f2e1aab472dd3e42f374a
federicolito <sironifede@gmail.com>
2020-08-26 22:16:00 -0300

    A las vistas les pase informacion para imprimir en los templates

--- DjangoThirdProject/Bibloteca/views.py
+++ DjangoThirdProject/Bibloteca/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
     
     context={}
     return render(request, 'Bibloteca/home.html',context)
 def librosView(request):
-    context= {}
+    libros = Libro.objects.all()
+    context= {'libros':libros}
+    
     return render(request, 'Bibloteca/libros.html', context)
 def alumnosView(request):
-    context= {}
+    alumnos = Alumno.objects.all()
+    context= {'alumnos':alumnos}
     return render(request, 'Bibloteca/alumnos.html', context)
